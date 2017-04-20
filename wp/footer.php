5bced0b94d75275d8e826eaebd99ffcf30c93fc5
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2017-04-20 17:50:13 +0200

    Footer fixed, icons fixed, wordpress ACF started

---
+++ wp/footer.php
@@ -0,0 +1,17 @@
+  <footer class="site-footer">
+    <div class="site-footer__wrapper">
+      <div class="contact-block">
+        <h3 class="contact-block__title">Контакты</h3>
+        <div class="contact">
+          <div class="contact__name">Телефон:</div>
+          <div class="contact__value"><a href="tel:+79114571223">+7 911 457-12-23</a></div>
+        </div>
+        <div class="contact">
+          <div class="contact__name">Email:</div>
+          <div class="contact__value"><a href="mailto: somemail@gmail.com">somemail@gmail.com</a></div>
+        </div>
+      </div>
+      <div class="copyrights">&copy;  <span class="copyrights--blue">Чистые Технологии</span> 2017. </div>
+    </div>
+  </footer>
+</body>
